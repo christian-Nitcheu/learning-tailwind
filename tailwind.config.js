654ccda047918bc8aa28473bf83620f14580ce42
christian-Nitcheu <christiannitcheu97@gmail.com>
2024-12-30 11:24:23 +0100

    mobile first

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'my-black': '#121826'
+      }
+    },
+  },
+  plugins: [],
+}
+
