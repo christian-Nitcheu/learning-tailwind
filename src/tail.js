94ab3d976d653b978a20718ff670f516bc3111ae
christian-Nitcheu <christiannitcheu97@gmail.com>
2025-01-14 14:54:47 +0100

    you

--- src/tail.js
+++ src/tail.js
@@ -3,15 +3,18 @@ let menuList = document.getElementById("menuList");
 let main = document.querySelector(".main");
 menu.addEventListener("click", () => {
     menuList.classList.remove("-top-full");
-    menuList.classList.add("top-5");
+    menuList.classList.add("top-0");
     console.log(menuList.classList);
+    main.style.backgroundColor = "black";
+    main.style.opacity = "0.25";
 })
 
 let closeMenuList = document.getElementById("closeMenuList");
 closeMenuList.addEventListener("click", () => {
-    menuList.classList.remove("top-5");
+    menuList.classList.remove("top-0");
     menuList.classList.add("-top-full");
-
+    main.style.backgroundColor = "transparent";
+    main.style.opacity = "1";
 });
 
 
