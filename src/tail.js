018f97f15251317f1576ad314f4bad20921986af
christian-Nitcheu <christiannitcheu97@gmail.com>
2025-01-14 14:34:03 +0100

    menulist

--- src/tail.js
+++ src/tail.js
@@ -4,15 +4,17 @@ let main = document.querySelector(".main");
 menu.addEventListener("click", () => {
     menuList.classList.remove("-top-full");
     menuList.classList.add("top-5");
-    main.style.backgroundColor = "black";
-    main.style.opacity = "0.25";
+    console.log(menuList.classList);
 })
 
 let closeMenuList = document.getElementById("closeMenuList");
 closeMenuList.addEventListener("click", () => {
     menuList.classList.remove("top-5");
     menuList.classList.add("-top-full");
-})
+
+});
+
+
 
 // window.addEventListener("click", (e) => {
 //     console.log(e.target);
